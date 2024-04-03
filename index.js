d6f97fdf7364fca26495515aae6a1996645f5549
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-04-03 17:30:58 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function superbowlWin(arrayRecord){
+    for(const rec of arrayRecord){
+        if(rec.result === "W"){
+            return (rec.year)
+        }
+    }
+}
+
+
+// function superbowlWin(element){
+//     if(element.result === "W"){
+//         return (element.year)
+//     }
+// }
+
+// record.find(superbowlWin)
